f9b4327b84cb3951c533a36d206840a31038f22b
MarcoGarciaa <margar306@gmail.com>
2024-02-19 14:22:50 +0100

    ...

--- app/src/main/java/com/example/proyectoprctica/recursos.kt
+++ app/src/main/java/com/example/proyectoprctica/recursos.kt
@@ -14,10 +14,11 @@ object DataProvider {
         Pair("Cine Negro", listOf(
             listOf(
                 listOf(R.drawable.laura__1944_film_poster_, R.drawable.lauraimagen, "Laura", "7.9", "El detective Mark McPherson investiga el asesinato de Laura Hunt, una ejecutiva neoyorquina que vivía rodeada de lujos. Durante la investigación, tratará con el mejor amigo de la fallecida, un columnista arrogante de carácter complicado. Poco a poco, el detective se involucra en exceso en la investigación y acaba por darse cuenta de que se está enamorando de la mujer asesinada","https://youtu.be/nzrm_vgoDX0?si=TO8z1HYwQN8FXoNP"),
-                listOf(R.drawable.laura__1944_film_poster_, R.drawable.lauraimagen, "The Asphalt Jungle", "7.8", "Un importante atraco se lleva a cabo según lo planeado, hasta que la mala suerte y las traiciones hacen que todo se venga abajo.","https://youtu.be/IXrP6Uo4nUI?si=3lYiwbR0twnR5u--"),
-                listOf(R.drawable.laura__1944_film_poster_, R.drawable.lauraimagen, "I Confess", "7.6", "Un sacerdote que respeta la santidad del confesionario, es enjuiciado por el asesinato cometido por uno de sus parroquianos. Para salvar su vida, el sacerdote se niega a revelar la confesión del verdadero asesino","https://youtu.be/NiajVY-f99s?si=RQ8y5v480yTbmarn"),
-                listOf(R.drawable.laura__1944_film_poster_, R.drawable.lauraimagen, "The Asphalt Jungle", "7.8", "",""),
-                listOf(R.drawable.laura__1944_film_poster_, R.drawable.lauraimagen, "The Asphalt Jungle", "7.8", "",""),
+                listOf(R.drawable.junglaasfalto1, R.drawable.junglaasfalto2, "The Asphalt Jungle", "7.8", "Un importante atraco se lleva a cabo según lo planeado, hasta que la mala suerte y las traiciones hacen que todo se venga abajo.","https://youtu.be/IXrP6Uo4nUI?si=3lYiwbR0twnR5u--"),
+                listOf(R.drawable.confieso171g8myktrpl__ac_uf894_1000_ql80_, R.drawable.confieso2450_1000, "I Confess", "7.6", "Un sacerdote que respeta la santidad del confesionario, es enjuiciado por el asesinato cometido por uno de sus parroquianos. Para salvar su vida, el sacerdote se niega a revelar la confesión del verdadero asesino","https://youtu.be/NiajVY-f99s?si=RQ8y5v480yTbmarn"),
+
+                listOf(R.drawable.laura__1944_film_poster_, R.drawable.lauraimagen, "The Asphalt Jungle", "7.8", "","")
+
             ))
 
         )
