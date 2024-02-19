03b7873e4b23c0fdcde98ecaedfd8251ec73234e
MarcoGarciaa <margar306@alu.centroafuera.es>
2024-02-19 19:22:20 +0100

    Initial commit

--- app/src/main/java/com/example/proyectoprctica/PerfilActivity.kt
+++ app/src/main/java/com/example/proyectoprctica/PerfilActivity.kt
@@ -3,7 +3,9 @@ package com.example.proyectoprctica
 import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -11,6 +13,8 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.firestore.FirebaseFirestore
@@ -22,12 +26,19 @@ class PerfilActivity  : AppCompatActivity()  {
     private lateinit var textViewTitulo3: com.google.android.material.textfield.TextInputEditText
     private lateinit var btnPelis: ImageButton
     private lateinit var btnSeries: ImageButton
-    private lateinit var btn1: ImageButton
-    private lateinit var btn2: ImageButton
-    private lateinit var btn3: ImageButton
-    private lateinit var ajustes: Button
+    private lateinit var btn1: Button
+    private lateinit var btn2: Button
+    private lateinit var btn3: Button
+    private lateinit var ajustes: ImageButton
     private lateinit var milinearLayout: View
     private lateinit var milinearLayout2: View
+
+    val pickMedia =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                imageView.setImageURI(uri)
+            }
+        }
 
     @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,6 +66,21 @@ class PerfilActivity  : AppCompatActivity()  {
             textViewTitulo3.setText( it.get("Contraseña") as String?)
 
         }
+
+
+        //CAMERA BUTTON
+        btn1.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, 1)
+            }
+        }
+
+        //GALLERY BUTTON
+       btn2.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
 
 
 
@@ -85,7 +111,21 @@ class PerfilActivity  : AppCompatActivity()  {
                 milinearLayout2.visibility = View.VISIBLE
             }
         }
+        btn1.setOnClickListener {
+
+         //   db.signOut();
+        }
 
 
+
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            val extras = data?.extras
+            val imgBitmap: Bitmap = extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imgBitmap)
+        }
     }
 }
