aa0ae96feb70e59d9cfc8b6bb011a7f0f8234358
MarcoGarciaa <margar306@gmail.com>
2024-02-20 09:00:01 +0100

    ...

--- app/src/main/java/com/example/proyectoprctica/recursos.kt
+++ app/src/main/java/com/example/proyectoprctica/recursos.kt
@@ -43,4 +43,41 @@ object DataProvider {
         ),
         )
 
+
+
+
+
+    val dataListsSeries: List<Pair<String, List<List<List<Any>>>>> = listOf(
+        Pair("Misterio", listOf(
+            listOf(
+                listOf(R.drawable.tiempo1, R.drawable.tiempo2, "Laura", "7.9", "El detective Mark McPherson investiga el asesinato de Laura Hunt, una ejecutiva neoyorquina que vivía rodeada de lujos. Durante la investigación, tratará con el mejor amigo de la fallecida, un columnista arrogante de carácter complicado. Poco a poco, el detective se involucra en exceso en la investigación y acaba por darse cuenta de que se está enamorando de la mujer asesinada","https://youtu.be/nzrm_vgoDX0?si=TO8z1HYwQN8FXoNP"),
+                listOf(R.drawable.detective1, R.drawable.detective2, "The Asphalt Jungle", "7.8", "Un importante atraco se lleva a cabo según lo planeado, hasta que la mala suerte y las traiciones hacen que todo se venga abajo.","https://youtu.be/IXrP6Uo4nUI?si=3lYiwbR0twnR5u--"),
+                listOf(R.drawable.turista1, R.drawable.turista2, "I Confess", "7.6", "Un sacerdote que respeta la santidad del confesionario, es enjuiciado por el asesinato cometido por uno de sus parroquianos. Para salvar su vida, el sacerdote se niega a revelar la confesión del verdadero asesino","https://youtu.be/NiajVY-f99s?si=RQ8y5v480yTbmarn"),
+                listOf(R.drawable.maas_allaa_de_la_duda_680662972_large, R.drawable.masalladudainvraisemblable_verite_03_g, "Más allá de la duda", "7.1", "Más allá de la duda es una película británica de 1956 con guion de Douglas Morrow, dirección de Fritz Lang y actuación de Dana Andrews y Joan Fontaine.","https://youtu.be/vv0JL5Oo7I8?si=mSzqiYg9QNdyhnUl")
+
+            ))
+
+        ),
+        Pair("Acción", listOf(
+            listOf(
+                listOf(R.drawable.mandalorian, R.drawable.mandalorian2, "The Warriors. Los amos de la noche", "7.0", "Verano de 1979, Nueva York. Se avecina una batalla campal entre las bandas callejeras de Nueva York, después de que se acuse por error a The Warriors del asesinato del líder de otra banda callejera","https://youtu.be/--gdB-nnQkU?si=Jv7m0bq3ozt8aWmR"),
+                listOf(R.drawable.carbon1, R.drawable.carbon2, "Escape from New York", "7.1", "En el año 1997 el avión del presidente de los Estados Unidos es secuestrado por un grupo radical. Sin embargo, los pasajeros consiguen sobrevivir y se encuentran solos en las calles de Nueva York, donde Manhattan se ha convertido en una prisión de alta seguridad","https://youtu.be/IXrP6Uo4nUI?si=3lYiwbR0twnR5u--"),
+                listOf(R.drawable.expense, R.drawable.expense2, "Jungla de Cristal", "7.2", "Un grupo terrorista se apodera de un edificio de Los Ángeles y toma a un grupo de personas como rehenes. El policía de Nueva York John McClane consigue escapar del acoso terrorista","https://youtu.be/GAdvN7KLc2o?si=jLRWBDhzWVadsFJn"),
+                listOf(R.drawable.desafio1, R.drawable.desafio2, "Desafío Total", "6.8", "La Tierra, año 2084. Doug Quaid, un hombre que lleva una vida aparentemente tranquila, vive atormentado por una pesadilla que todas las noches lo transporta a Marte","https://youtu.be/25OjH_B-Nfw?si=1hTfkdSlTk9Ei7SW")
+
+            ))
+
+        ),
+        Pair("Comedia", listOf(
+            listOf(
+                listOf(R.drawable.aquinohay, R.drawable.aquinohay2, "La vida de Brian", "8.0", "Durante la época bíblica, un hombre parece ser el Mesías y se ve puesto como líder de un movimiento religioso. Su vida estará marcada por su castrante madre, sus nuevos amigos del Frente Popular de Judea y su novia feminista","https://youtu.be/y8YyfvINTWc?si=El2GtAlrOyJuun13"),
+                listOf(R.drawable.aida, R.drawable.aida2, "Sucedió una noche", "8.1", "Una rica heredera escapa del yate en el que la ha recluido su padre y huye en un viaje en el que conocerá a un periodista que cambiará su vida","https://youtu.be/IXrP6Uo4nUI?si=3lYiwbR0twnR5u--"),
+                listOf(R.drawable.machos, R.drawable.machos2, "Jungla de Cristal", "8.3", "Dos cantantes y bailarinas de Arkansas se embarcan en un crucero hacia París, donde vivirán mil aventuras amorosas. El mito Marilyn, en lo más alto","https://youtu.be/97TYs2YXbJw?si=Qz5-4rtRsrSW6buf"),
+                listOf(R.drawable.apartamento, R.drawable.apartamento2, "El apartamento", "9.1", "C.C.Baxter, empleado en una compañía de seguros, intenta ascender en la empresa prestando la llave de su apartamento para las citas de sus jefes.","https://youtu.be/25OjH_B-Nfw?si=1hTfkdSlTk9Ei7SW")
+
+            ))
+
+        ),
+    )
+
 }
